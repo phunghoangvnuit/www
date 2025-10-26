aa95c9f6ffdb7bd827fd34763a591bd68dac2f5f
phunghoangvnuit <phunghoangvnuit@gmail.com>
2025-10-26 09:14:37 +0700

    update 261025.0915pm.nav

--- client-side/js/nav.js
+++ client-side/js/nav.js
@@ -1,27 +1,9 @@
-// Handle language hover
-document.querySelectorAll('.nav-item strong').forEach(item => {
-  item.addEventListener('mouseenter', () => {
-    item.classList.add('fade-out');
-    setTimeout(() => {
-      item.textContent = item.dataset.en;
-      item.classList.remove('fade-out');
-    }, 70);
-  });
-
-  item.addEventListener('mouseleave', () => {
-    item.classList.add('fade-out');
-    setTimeout(() => {
-      item.textContent = item.dataset.vi;
-      item.classList.remove('fade-out');
-    }, 70);
-  });
-});
-
-// Handle active tab click
 const navLinks = document.querySelectorAll('.nav-item.nav-link');
+
 navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    navLinks.forEach(l => l.classList.remove('active')); // remove old
-    link.classList.add('active'); // set new active
+  link.addEventListener('click', e => {
+    e.preventDefault(); // prevent page reload
+    navLinks.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
   });
 });
